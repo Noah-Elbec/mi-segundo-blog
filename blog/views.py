ef75e15bbea230fd8975bff8ef8da625fcd57177
noah elbec <noah_elbec@hotmail.com>
2018-03-26 00:35:52 -0500

    Modificando la plantilla html para mostrar datos de la base de datos

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 def post_list(request):
-    return render(request,'blog/post_list.html',{})
+    posts = Post.objects.filter(fecha_de_publicacion__lte = timezone.now()).order_by('-fecha_de_publicacion')
+    return render(request,'blog/post_list.html',{'posts':posts})
     
